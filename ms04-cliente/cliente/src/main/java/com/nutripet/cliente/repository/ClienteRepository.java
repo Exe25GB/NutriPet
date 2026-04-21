7e1954bc94f5e09d5f925594c16544ccf7787931
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-04-21 11:32:22 -0400

    Guardando la refactorizacion

--- ms04-cliente/cliente/src/main/java/com/nutripet/cliente/repository/ClienteRepository.java
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/repository/ClienteRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nutripet.cliente.main.Cliente;
+import com.nutripet.cliente.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
