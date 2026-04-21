88d937b755492a164e685633c3472952418c22c0
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-04-21 11:32:22 -0400

    Guardando la refactorizacion

--- ms04-cliente/cliente/src/main/java/com/nutripet/cliente/service/ClienteService.java
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/service/ClienteService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.nutripet.cliente.main.Cliente;
+import com.nutripet.cliente.model.Cliente;
 import com.nutripet.cliente.repository.ClienteRepository;
 
 import lombok.RequiredArgsConstructor;
