4bd5848312956e7a93221a03c6c062e0f402b158
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-04-26 23:52:23 -0400

    Avanzando con el ms05 y creando una classe enum que contiene dos opciones "MINORISTA" y "MAYORISTA" asignando valor no posicion.

--- ms04-cliente/cliente/src/main/java/com/nutripet/cliente/model/Cliente.java
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/model/Cliente.java
@@ -2,12 +2,13 @@ package com.nutripet.cliente.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,12 +25,13 @@ public class Cliente {
     @Column(name = "id_cliente")
     private Long idCliente;
 
-    //id_usuario (FK)
+    //>id_usuario< es una clave forania de la Clase Usuario del ms01-usuario
 
     // True = Mayorista, False = Minorista
-    @NotNull(message = "Debe especificar si el cliente es mayorista(T) o minorista(F)")
+    @NotBlank(message = "Se tiene que definir si es 'MINORISTA' o 'MAYORISTA'")
+    @Enumerated(EnumType.STRING)
     @Column(name = "tipo_cliente", nullable = false)
-    private boolean tipoCliente;
+    private TipoCliente tipoCliente;
 
     @Size(max = 100, message = "La razón social no puede exceder los 100 caracteres")
     @Column(name = "razon_social", nullable = true, length = 100)
