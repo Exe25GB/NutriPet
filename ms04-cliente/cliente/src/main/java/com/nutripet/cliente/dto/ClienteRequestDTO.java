ea1831319b1bb8d4818d5fd28b141aa37cefa5f7
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-04-28 10:47:32 -0400

    Creando el DTO de ms04-cliente y asignando la clave foranea a la classe cliente con usuario (ms01)

---
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/dto/ClienteRequestDTO.java
@@ -0,0 +1,25 @@
+package com.nutripet.cliente.dto;
+
+import com.nutripet.cliente.model.TipoCliente;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteRequestDTO {
+    @NotBlank(message = "Se tiene que definir si es 'MINORISTA' o 'MAYORISTA'")
+    private TipoCliente tipoCliente;
+
+    @Size(max = 100, message = "La razón social no puede exceder los 100 caracteres")
+    private String razonSocial;
+
+    @NotBlank(message = "La direccion de despacho es obligatorio")
+    @Size(max = 250, message = "La dirrecion es demasiado larga")
+    private String dirrecionDespacho;
+
+}
--- ms04-cliente/cliente/src/main/java/com/nutripet/cliente/model/Cliente.java
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/model/Cliente.java
@@ -7,9 +7,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,19 +25,17 @@ public class Cliente {
     private Long idCliente;
 
     //>id_usuario< es una clave forania de la Clase Usuario del ms01-usuario
+    @JoinColumn(name = "id_usuario", nullable = false)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
 
-    // True = Mayorista, False = Minorista
-    @NotBlank(message = "Se tiene que definir si es 'MINORISTA' o 'MAYORISTA'")
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo_cliente", nullable = false)
     private TipoCliente tipoCliente;
 
-    @Size(max = 100, message = "La razón social no puede exceder los 100 caracteres")
     @Column(name = "razon_social", nullable = true, length = 100)
     private String razonSocial;
 
-    @NotBlank(message = "La direccion de despacho es obligatorio")
-    @Size(max = 250, message = "La dirrecion es demasiado larga")
     @Column(name = "direccio_despacho", nullable = false, length = 250)
     private String direccionDespacho;
 
