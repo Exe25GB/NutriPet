ce938aefb281c2e07f0b3aa740c4de94e922d750
Victor <217826359+Hexatriss@users.noreply.github.com>
2026-05-04 09:04:59 -0400

    Creación de modelo para el ms Producto

---
+++ ms02-producto/producto/src/main/java/com/nutripet/producto/model/Producto.java
@@ -0,0 +1,48 @@
+package com.nutripet.producto.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idProducto;
+
+    @Column(nullable = false)
+    private String skuProducto;
+
+    @Column(nullable = false)
+    private String nombreProducto;
+
+    @Column(nullable = false)
+    private BigDecimal pesoProducto;
+
+    //Descripción de producto.
+    @Column(nullable = false)
+    private String descProducto;
+    
+    @JoinColumn(nullable = false)
+    private long idCategoria;
+
+
+
+    
+
+
+}
