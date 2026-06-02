f429348ac3ff46a406f23ac95cff377b9407f6d6
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-06-02 12:12:56 -0400

    Implementando Swagger en el micro servicio ms05-precio (funcionando)

--- ms05-precio/precio/src/main/java/com/nutripet/precio/controller/PrecioController.java
+++ ms05-precio/precio/src/main/java/com/nutripet/precio/controller/PrecioController.java
@@ -10,17 +10,21 @@ import com.nutripet.precio.dto.PrecioRequestDTO;
 import com.nutripet.precio.dto.PrecioResponseDTO;
 import com.nutripet.precio.service.PrecioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/precios")
 @RequiredArgsConstructor
+@Tag(name = "Pago", description = "Manipulacion Pagos")
 public class PrecioController {
 
     private final PrecioService precioService;
 
     @PostMapping("/calcular")
+    @Operation(summary = "peticiones Post - Precios", description = "Plata")
     public ResponseEntity<PrecioResponseDTO> calcularPrecio(@Valid @RequestBody PrecioRequestDTO request) {
         return ResponseEntity.ok(precioService.calcularPrecioFinal(request));
     }
