c038bd2fcee6b9541cb920ded48ecba7dcba24a1
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-06-08 09:22:59 -0400

    Implementando Swagger en el micro servicio ms07-pago (funcionando)

--- ms07-pago/pago/src/main/java/com/nutripet/pago/controller/PagoController.java
+++ ms07-pago/pago/src/main/java/com/nutripet/pago/controller/PagoController.java
@@ -10,17 +10,21 @@ import com.nutripet.pago.dto.PagoRequestDTO;
 import com.nutripet.pago.dto.PagoResponseDTO;
 import com.nutripet.pago.service.PagoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/pagos")
 @RequiredArgsConstructor
+@Tag(name = "Pago", description = "Procesa el pago")
 public class PagoController {
 
     private final PagoService pagoService;
 
     @PostMapping("/procesar")
+    @Operation(summary = "Ver voletas", description = "Muestra la lista de pagos de la BD")
     public ResponseEntity<PagoResponseDTO> procesarPago(@Valid @RequestBody PagoRequestDTO request) {
         return ResponseEntity.status(201).body(pagoService.procesarPago(request));
     }
