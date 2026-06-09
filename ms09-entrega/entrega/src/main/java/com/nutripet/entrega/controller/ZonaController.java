22ec68a0a3707794bfccba937af5bbd649aad1ee
Víctor <217826359+Svvtfs@users.noreply.github.com>
2026-06-09 11:11:21 -0400

    Se agrego swagger en los controladores del micro-servicio nueve.

--- ms09-entrega/entrega/src/main/java/com/nutripet/entrega/controller/ZonaController.java
+++ ms09-entrega/entrega/src/main/java/com/nutripet/entrega/controller/ZonaController.java
@@ -2,6 +2,9 @@ package com.nutripet.entrega.controller;
 
 import com.nutripet.entrega.model.Zona;
 import com.nutripet.entrega.service.ZonaService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,16 +16,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/zonas")
 @RequiredArgsConstructor
+@Tag(name = "Zona", description = "Operaciones relacionado a zonas.")
 public class ZonaController {
 
     private final ZonaService zonaService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos las zonas.", description = "Lista todos los datos de zonas.")
     public ResponseEntity<List<Zona>> listarTodas() {
         return ResponseEntity.ok(zonaService.obtenerTodas());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener zona por id.", description = "Obtiene una zona en especifico buscando por su id.")
     public ResponseEntity<Zona> buscarPorId(@PathVariable Long id) {
         return zonaService.obtenerPorId(id)
                 .map(ResponseEntity::ok)
@@ -30,12 +36,14 @@ public class ZonaController {
     }
 
     @PostMapping
+    @Operation(summary = "Crear zona", description = "Crear o agregar a traves de registro de datos uno o mas zonas.")
     public ResponseEntity<Zona> crear(@Valid @RequestBody Zona zona) {
         Zona nuevaZona = zonaService.guardar(zona);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaZona);
     }
 
     @PutMapping("/{id}")
+        @Operation(summary = "Actualizar zona", description = "Actualiza o modifica un registro de zona en especifico por id.")
     public ResponseEntity<Zona> actualizar(@PathVariable Long id, @Valid @RequestBody Zona zona) {
         return zonaService.obtenerPorId(id)
                 .map(existente -> {
@@ -46,6 +54,7 @@ public class ZonaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar zona", description = "Elimina permanentemente un registro de zona por id. Puede ser uno o mas.")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         return zonaService.obtenerPorId(id)
                 .map(zona -> {
