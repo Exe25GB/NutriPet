0c218b1ea77006914253ba4aea543f4c1b135978
Exequiel G <217678734+Exe25GB@users.noreply.github.com>
2026-06-14 02:18:27 -0400

    Mas funciones de HATEOS para el ms04-Cliente

--- ms04-cliente/cliente/src/main/java/com/nutripet/cliente/assemblers/ClienteModelAssembler.java
+++ ms04-cliente/cliente/src/main/java/com/nutripet/cliente/assemblers/ClienteModelAssembler.java
@@ -8,15 +8,26 @@ import org.springframework.stereotype.Component;
 
 import com.nutripet.cliente.controller.ClienteControllerV2;
 import com.nutripet.cliente.dto.ClienteResponseDTO;
+import com.nutripet.cliente.dto.DireccionResponseDTO;
 
 @Component
 public class ClienteModelAssembler implements RepresentationModelAssembler<ClienteResponseDTO, EntityModel<ClienteResponseDTO>>{
+    
     @Override
-    public EntityModel<ClienteResponseDTO> toModel(ClienteResponseDTO cliente){
+    public EntityModel<ClienteResponseDTO> toModel(ClienteResponseDTO cliente) {
         return EntityModel.of(cliente,
-            // linkTo(methodOn(ClienteControllerV2.class).getClienteByCodigo(cliente.getIdCliente())).withSelfRel(),
-            linkTo(methodOn(ClienteControllerV2.class).getAllCliente()).withRel("clientes")
-            );
+                // Todos los clientes
+                linkTo(methodOn(ClienteControllerV2.class).getAllCliente()).withRel("clientes")
+        );
+    }
+
+    public EntityModel<DireccionResponseDTO> toDireccionModel(DireccionResponseDTO direccion) {
+        return EntityModel.of(direccion,
+                // Direccion especifica
+                linkTo(methodOn(ClienteControllerV2.class).getDireccionById(direccion.getIdDireccion())).withSelfRel(),
+                // Todas las direcciones de un cliente específico
+                linkTo(methodOn(ClienteControllerV2.class).getDireccionesByClienteId(direccion.getIdCliente())).withRel("direccionesCliente")
+        );
     }
 
 }
