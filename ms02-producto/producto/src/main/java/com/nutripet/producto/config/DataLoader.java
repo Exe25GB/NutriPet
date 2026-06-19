58fbcc3509353b7bd5daec5fd9d687ad4175bbb6
Víctor <217826359+Svvtfs@users.noreply.github.com>
2026-06-18 22:53:23 -0400

    Implementacion de DataFaker con una conexión de base de datos  nueva con Laragon en el micro-servicio 02.

---
+++ ms02-producto/producto/src/main/java/com/nutripet/producto/config/DataLoader.java
@@ -0,0 +1,69 @@
+package com.nutripet.producto.config;
+
+import com.nutripet.producto.model.Categoria;
+import com.nutripet.producto.model.Producto;
+import com.nutripet.producto.repository.CategoriaRepository;
+import com.nutripet.producto.repository.ProductoRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Random;
+
+@Profile("dev")
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository CategoriaRepository;
+
+    @Autowired
+    private ProductoRepository ProductoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Faker faker = new Faker();
+        Random random = new Random();
+
+        System.out.println("Iniciando la carga de datos falsos con DataFaker...");
+
+
+        for (int i = 0; i < 5; i++) {
+            Categoria categoria = new Categoria();
+            categoria.setNombre(faker.commerce().department()); 
+            CategoriaRepository.save(categoria);
+        }
+
+        List<Categoria> categorias = CategoriaRepository.findAll();
+
+
+        for (int i = 0; i < 20; i++) {
+            Producto producto = new Producto();
+            
+            producto.setSku(faker.code().asin()); 
+            
+            producto.setNombre(faker.commerce().productName());
+            
+            double pesoAleatorio = faker.number().randomDouble(2, 1, 30);
+            producto.setPeso(BigDecimal.valueOf(pesoAleatorio));
+            
+            producto.setDescripcion(faker.lorem().sentence());
+            
+            producto.setCategoria(categorias.get(random.nextInt(categorias.size())));
+            
+            producto.setTipoMascota(faker.options().option("Perro", "Gato", "Ave", "Exótico", "Roedor"));
+            producto.setCicloVital(faker.options().option("Cachorro", "Adulto", "Senior", "Todas las edades"));
+            
+            producto.setMarca(faker.company().name());
+
+            ProductoRepository.save(producto);
+        }
+
+        System.out.println("¡Carga de datos finalizada! Se insertaron 5 categorías y 20 productos.");
+    }
+}
